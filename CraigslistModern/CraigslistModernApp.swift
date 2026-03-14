4041caaab5a48b20732e546d47ee95c583ac9d00
CoriyonArrington <coriyonarrington@gmail.com>
2026-03-14 02:27:35 -0500

    Initial Commit

---
+++ CraigslistModern/CraigslistModernApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraigslistModernApp.swift
+//  CraigslistModern
+//
+//  Created by Coriyon Arrington on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CraigslistModernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CraigslistModernTests/CraigslistModernTests.swift
@@ -0,0 +1,17 @@
+//
+//  CraigslistModernTests.swift
+//  CraigslistModernTests
+//
+//  Created by Coriyon Arrington on 3/14/26.
+//
+
+import Testing
+@testable import CraigslistModern
+
+struct CraigslistModernTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
